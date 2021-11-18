7c0051366df01dc63049510f4e56ebd28ad1a517
Luuk van Kooten <luuk@Mac-mini-van-Luuk.local>
2021-11-18 15:15:48 +0100

    Create http server with express js

---
+++ packages/server/README.md
@@ -0,0 +1,13 @@
+# Server
+
+
+## Scripts
+
+### `npm run start`
+
+Start the server for development purpose.
+
+### `npm run build`
+
+Build the to output
+
---
+++ packages/server/server.ts
@@ -0,0 +1,14 @@
+import express from 'express';
+
+const app = express();
+const port = 3000;
+
+console.log(process.env);
+
+app.get('/', (req, res) => {
+	res.send('Hello World!');
+})
+
+app.listen(port, () => {
+	console.log(`Example app listening at http://localhost:${port}`);
+})
