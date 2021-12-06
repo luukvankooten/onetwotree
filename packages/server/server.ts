2ac3e3037d733e320823bd82dc08e39aefc29114
Luuk van Kooten <luuk@Mac-mini-van-Luuk.local>
2021-12-06 18:41:01 +0100

    Changed cors

--- packages/server/server.ts
+++ packages/server/server.ts
@@ -1,14 +1,9 @@
-import { isTokenExpired, User } from "@12tree/domain";
-import express, { Request } from "express";
+import express from "express";
 import passport from "passport";
-import { Profile, Strategy as SpotifyStrategy } from "passport-spotify";
-import jwt from "jsonwebtoken";
 import path from "path";
 import cors from "cors";
 import { commentControllers, userControllers } from "./src/v1";
 import authController from "./src/auth";
-import { userRepo } from "./src";
-import nodeFetch from "node-fetch";
 
 const app = express();
 const port = process.env.PORT || 3000;
@@ -18,7 +13,7 @@ const host = process.env.BACKEND_URL;
 const client_id = process.env.SPOTIFY_CLIENT_ID || "";
 const client_secret = process.env.SPOTIFY_CLIENT_SECRET || "";
 
-app.use(cors({ origin: process.env.BACKEND_URL }));
+app.use(cors({ origin: process.env.FRONTEND_URL }));
 app.use(passport.initialize());
 app.use(express.json());
 app.use("/auth", authController);
