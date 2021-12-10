497af5c5d20c912da0afcf6d4d2119d5e80eaf8a
Luuk van Kooten <luuk@Mac-mini-van-Luuk.local>
2021-12-10 12:51:30 +0100

    Refresh token if is expired

--- packages/infrastructure/src/connections/spotify.ts
+++ packages/infrastructure/src/connections/spotify.ts
@@ -1,4 +1,7 @@
 import SpotifyWebApi from "spotify-web-api-node";
+import { Token, isTokenExpired } from "@12tree/domain";
+
+let token: Token | undefined = undefined;
 
 const api = new SpotifyWebApi({
   clientId: process.env.SPOTIFY_CLIENT_ID,
@@ -6,8 +9,15 @@ const api = new SpotifyWebApi({
 });
 
 const spotify = async () => {
-  if (!api.getAccessToken()) {
+  if (!token || (token && isTokenExpired(token))) {
     const data = await api.clientCredentialsGrant();
+
+    token = {
+      accessToken: data.body["access_token"],
+      refreshToken: "",
+      expiresIn: data.body["expires_in"],
+      createdAt: Date.now(),
+    };
 
     api.setAccessToken(data.body["access_token"]);
   }
