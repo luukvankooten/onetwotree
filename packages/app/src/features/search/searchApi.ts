002862dceb8bfb05969ad4eb7a7ebaf44e6ba25c
luukvankooten <luukvankooten@gmail.com>
2022-04-10 12:42:56 +0200

    Fix localhost error

--- packages/app/src/features/search/searchApi.ts
+++ packages/app/src/features/search/searchApi.ts
@@ -1,20 +1,9 @@
 import { SearchTrack } from "@12tree/domain/src/entities/track";
+import ApiFetch from "../../utils/apiFetch";
 
 export async function searchTrackApi(
   query: string,
   auth: string
 ): Promise<SearchTrack[]> {
-  const myHeaders = new Headers();
-  myHeaders.append("Authorization", `Bearer ${auth}`);
-
-  const response = await fetch(
-    `http://localhost:4000/api/v1/tracks/search?q=${query}`,
-    {
-      method: "GET",
-      headers: myHeaders,
-      redirect: "follow",
-    }
-  );
-
-  return (await response.json()) as SearchTrack[];
+  return await ApiFetch<SearchTrack[]>(auth, `tracks/search?q=${query}`);
 }
